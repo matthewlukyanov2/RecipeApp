74c3ca6f5a5a788d95a4cc3b20921fdb378f0254
matthewlukyanov2 <brucelukyanov@gmail.com>
2024-11-26 18:11:30 +0000

    Initial commit of RecipeApp

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config(); // Allows us to use the `.env` file
+
+const app = express(); // Initialize Express
+const PORT = process.env.PORT || 4000; // Set the server port
+
+// Middleware
+app.use(cors()); // Enable Cross-Origin Resource Sharing
+app.use(bodyParser.json()); // Parse JSON bodies
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+// Connect to MongoDB
+mongoose
+    .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        // Start the server after successful database connection
+        app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    }) 
+    .catch(err => console.log(err));
+
+
