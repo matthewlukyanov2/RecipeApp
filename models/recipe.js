2561855107ae1e36672f4469fc4719a4b9074755
matthewlukyanov2 <brucelukyanov@gmail.com>
2024-11-27 17:18:55 +0000

    added contents to recipe.js, defined schema for my recipe app and exported the model

---
+++ models/recipe.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+// Defining schema for my recipe app
+const RecipeSchema = new mongoose.Schema({
+    title: { type: String, required: true },          // Recipe title
+    ingredients: { type: [String], required: true },  // List of ingredients
+    instructions: { type: String, required: true },   // Cooking instructions
+    cookingTime: { type: Number, required: true },    // Cooking time in minutes
+});
+
+// Create and export the Recipe model
+const Recipe = mongoose.model('Recipe', RecipeSchema);
+
+module.exports = Recipe;
