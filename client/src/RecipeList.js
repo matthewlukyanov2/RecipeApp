cdecb02d59ebd0c9952dbf52ff01b953c38a0556
matthewlukyanov2 <brucelukyanov@gmail.com>
2024-11-29 21:18:11 +0000

    Added state to manage recipes and loading indicator

--- client/src/RecipeList.js
+++ client/src/RecipeList.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+const RecipeList = () => {
+    const [recipes, setRecipes] = useState([]);  // State to store recipes
+    const [loading, setLoading] = useState(true); 
+}  
+
 
 
 export default RecipeList;
