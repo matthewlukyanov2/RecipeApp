6c16b1771b5da11aa4e7754c348b7bb606d08bb9
matthewlukyanov2 <brucelukyanov@gmail.com>
2024-12-02 17:02:06 +0000

    imported AddRecipe to app.js so you can add recipe to console

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import RecipeList from './RecipeList';
+import AddRecipe from './AddRecipe';
 
 function App() {
   return (
     <div className="App">
        <header className="App-header">
-        <h1>Welcome to the Recipe App</h1>
+       <h1>Recipe App</h1>
+        {/* AddRecipe Component */}
+        <AddRecipe />
+        {/* RecipeList Component */}
+        <RecipeList />
       </header>
-      <main>
-        <RecipeList /> {/* Render the RecipeList component */}
-      </main>
     </div>
   );
 }
