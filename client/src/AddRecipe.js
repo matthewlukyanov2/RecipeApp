3cb07d3da9e095992367f671647813fabb612ff9
matthewlukyanov2 <brucelukyanov@gmail.com>
2024-12-02 16:55:37 +0000

    Added form structure for adding new recipes in AddRecipe component

--- client/src/AddRecipe.js
+++ client/src/AddRecipe.js
@@ -6,7 +6,7 @@ const AddRecipe = () => {
     const [ingredients, setIngredients] = useState('');
     const [instructions, setInstructions] = useState('');
     const [cookingTime, setCookingTime] = useState('');
-}
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -30,6 +30,53 @@ const AddRecipe = () => {
           console.error('Error adding recipe:', error);
         });
     };
+
+    return (
+        <div>
+          <h2>Add a New Recipe</h2>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label>Title</label>
+              <input 
+                type="text" 
+                value={title} 
+                onChange={(e) => setTitle(e.target.value)} 
+                required
+              />
+            </div>
+            <div>
+              <label>Ingredients (comma separated)</label>
+              <input 
+                type="text" 
+                value={ingredients} 
+                onChange={(e) => setIngredients(e.target.value)} 
+                required
+              />
+            </div>
+            <div>
+              <label>Instructions</label>
+              <textarea 
+                value={instructions} 
+                onChange={(e) => setInstructions(e.target.value)} 
+                required
+              />
+            </div>
+            <div>
+              <label>Cooking Time (minutes)</label>
+              <input 
+                type="number" 
+                value={cookingTime} 
+                onChange={(e) => setCookingTime(e.target.value)} 
+                required
+              />
+            </div>
+            <button type="submit">Add Recipe</button>
+          </form>
+        </div>
+      );
+    };
+    
+    export default AddRecipe;
 
 
 
