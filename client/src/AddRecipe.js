40c161d280e9a29b20476c9eb78c30d91f4db338
matthewlukyanov2 <brucelukyanov@gmail.com>
2024-12-02 16:11:46 +0000

    Initialized state variables in AddRecipe.js for handling form inputs

--- client/src/AddRecipe.js
+++ client/src/AddRecipe.js
@@ -1,2 +1,9 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+
+const AddRecipe = () => {
+    const [title, setTitle] = useState('');
+    const [ingredients, setIngredients] = useState('');
+    const [instructions, setInstructions] = useState('');
+    const [cookingTime, setCookingTime] = useState('');
+}
