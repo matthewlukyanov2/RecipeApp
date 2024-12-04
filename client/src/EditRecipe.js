12db3af178862c81b9c0a2a9e40dea0b5693b7b6
matthewlukyanov2 <brucelukyanov@gmail.com>
2024-12-04 20:09:44 +0000

    Add handleSave function to update recipe details via API

--- client/src/EditRecipe.js
+++ client/src/EditRecipe.js
@@ -9,4 +9,13 @@ const EditRecipe = ({ recipeId, onCancel, onUpdate }) => {
             .then(response => setRecipe(response.data))
             .catch(error => console.error('Error fetching recipe:', error));
     }, [recipeId]);
+
+    const handleSave = () => {
+        axios.put(`http://localhost:4000/api/recipes/${recipeId}`, recipe)
+            .then(response => {
+                console.log('Recipe updated:', response.data);
+                onUpdate(response.data); 
+            })
+            .catch(error => console.error('Error updating recipe:', error));
+    };
 }
